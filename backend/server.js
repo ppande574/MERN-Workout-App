f41e5393cf22ec33394c4fc7bde5c9e2baf9d1f1
ppande574 <ppande0221@gmail.com>
2024-01-22 14:47:02 -0600

    Added env variable package and initialization

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
+require('dotenv').config() // Configures app to use the added dotenv package
+
 const express = require('express')
 
 // express app
 const app = express()
 
-//listen for requests at a particular port number
+//listen for requests at a particular port number stored as an env variable
 
-app.listen(4000, () => {
-    console.log("Listening on port 4000")
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port ", process.env.PORT);
 })
 
 // responding to GET requests (the route goes in the parentheses for now '/' specifies the root which is just localhost:4000)
